360936147195c1669a3c84ed71ea31f0a981b6e6
Alex <AlexiyKami@github.com>
2022-07-02 17:27:46 +0300

    feat: add types and enum

--- migration-to-ts/src/types/index.ts
+++ migration-to-ts/src/types/index.ts
@@ -1,34 +1,48 @@
+export enum statusCode {
+    badRequest = 400,
+    unauthorized,
+    notFound = 404,
+    tooManyRequests = 429,
+    serverError = 500,
+}
+
+export type VoidCallback<T> = (data?: T) => void;
+
+export type UrlOptions = {
+    [prop: string]: string;
+};
+
 export interface IDataArticles {
-  status: string;
-  totalResults: number;
-  articles: IArticle[];
+    status: string;
+    totalResults: number;
+    articles: IArticle[];
 }
 
 export interface IDataSources {
-  sources: ISource[];
-  status: string;
+    sources: ISource[];
+    status: string;
 }
 
 export interface IArticle {
-  author: string;
-  content: string;
-  description: string;
-  publishedAt: string;
-  source: {
-      id: string;
-      name: string;
-  };
-  title: string;
-  url: string;
-  urlToImage: string;
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: {
+        id: string;
+        name: string;
+    };
+    title: string;
+    url: string;
+    urlToImage: string;
 }
 
 export interface ISource {
-  category: string;
-  country: string;
-  description: string;
-  id: string;
-  language: string;
-  name: string;
-  url: string;
+    category: string;
+    country: string;
+    description: string;
+    id: string;
+    language: string;
+    name: string;
+    url: string;
 }
