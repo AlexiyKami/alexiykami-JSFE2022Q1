0f3acd2e7dc78339b3dcceaab39294d840d4b70b
Alex <AlexiyKami@github.com>
2022-07-02 17:35:53 +0300

    refactor: migration app.js to typescript

---
+++ migration-to-ts/src/components/app/app.ts
@@ -0,0 +1,21 @@
+import { IDataArticles, IDataSources } from '../../types/index';
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    private controller: AppController;
+    private view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    public start() {
+        (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e) =>
+            this.controller.getNews<IDataArticles>(e, (data) => this.view.drawNews(data as IDataArticles))
+        );
+        this.controller.getSources<IDataSources>((data) => this.view.drawSources(data as IDataSources));
+    }
+}
+
+export default App;
