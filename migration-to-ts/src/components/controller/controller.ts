f5fc188d9c5120fc85bd8bd538ce66e99a9d99bd
Alex <AlexiyKami@github.com>
2022-07-02 17:35:38 +0300

    refactor: migration controller.js to typescript

---
+++ migration-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,40 @@
+import { VoidCallback } from './../../types/index';
+import AppLoader from './appLoader';
+
+class AppController extends AppLoader {
+    public getSources<T>(callback: VoidCallback<T>) {
+        super.getResp<T>(
+            {
+                endpoint: 'sources',
+            },
+            callback
+        );
+    }
+
+    public getNews<T>(e: Event, callback: VoidCallback<T>) {
+        let target: HTMLElement = e.target as HTMLElement;
+        const newsContainer: HTMLElement = e.currentTarget as HTMLElement;
+
+        while (target !== newsContainer) {
+            if (target.classList.contains('source__item')) {
+                const sourceId: string | null = target.getAttribute('data-source-id');
+                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                    newsContainer.setAttribute('data-source', sourceId as string);
+                    super.getResp<T>(
+                        {
+                            endpoint: 'everything',
+                            options: {
+                                sources: sourceId,
+                            },
+                        },
+                        callback
+                    );
+                }
+                return;
+            }
+            target = target.parentNode as HTMLElement;
+        }
+    }
+}
+
+export default AppController;
