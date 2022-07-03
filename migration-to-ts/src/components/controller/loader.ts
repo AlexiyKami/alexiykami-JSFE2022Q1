9e044450d1c0a9d6fba724a715f1d5bcbe36ca9f
Alex <AlexiyKami@github.com>
2022-07-03 17:57:49 +0300

    refactor: use reduce method, new statusCode check and remove redundant type

--- migration-to-ts/src/components/controller/loader.ts
+++ migration-to-ts/src/components/controller/loader.ts
@@ -19,8 +19,9 @@ class Loader {
 
     private errorHandler(res: Response) {
         if (!res.ok) {
-            if (res.status === statusCode.unauthorized || res.status === statusCode.notFound)
+            if (Object.keys(statusCode).includes(res.status.toString())) {
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            }
             throw Error(res.statusText);
         }
 
@@ -29,19 +30,19 @@ class Loader {
 
     private makeUrl(options: UrlOptions, endpoint: string) {
         const urlOptions: UrlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+        const url = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
-        });
-
-        return url.slice(0, -1);
+        return Object.keys(urlOptions)
+            .reduce((total, curr) => {
+                return total + `${curr}=${urlOptions[curr]}&`;
+            }, url)
+            .slice(0, -1);
     }
 
     private load<T>(method: string, endpoint: string, callback: VoidCallback<T>, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
-            .then((res) => (res as Response).json())
+            .then((res) => res.json())
             .then((data: T) => callback(data))
             .catch((err) => console.error(err));
     }
