85a1cc7f133f23f3d8cb34ba6c6051cad5839d41
Alex <AlexiyKami@github.com>
2022-07-03 15:56:21 +0300

    refactor: remove redundant property checking

--- migration-to-ts/src/components/view/appView.ts
+++ migration-to-ts/src/components/view/appView.ts
@@ -1,4 +1,3 @@
-import { IArticle, ISource } from './../../types/index';
 import { IDataArticles, IDataSources } from '../../types/index';
 import News from './news/news';
 import Sources from './sources/sources';
@@ -12,13 +11,11 @@ export class AppView {
     }
 
     public drawNews(data: Readonly<IDataArticles>) {
-        const values : IArticle[] = data?.articles ? data?.articles : [];
-        this.news.draw(values);
+        this.news.draw(data.articles);
     }
 
     public drawSources(data: Readonly<IDataSources>) {
-        const values : ISource[] = data?.sources ? data?.sources : [];
-        this.sources.draw(values);
+        this.sources.draw(data.sources);
     }
 }
 
