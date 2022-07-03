dfc01b77114a22b3202e978ca59386521392c86e
Alex <AlexiyKami@github.com>
2022-07-03 14:35:14 +0300

    feat: add CopyPlugin in webpack.config.js

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
@@ -35,6 +36,18 @@ const baseConfig = {
         new CleanWebpackPlugin(),
         new EslingPlugin({
             extensions: 'ts'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'img', 'rss.svg'),
+                    to: path.resolve(__dirname, 'dist', 'img', 'rss.svg'),
+                },
+                {
+                    from: path.resolve(__dirname, 'src', 'img', 'news_placeholder.jpg'),
+                    to: path.resolve(__dirname, 'dist', 'img', 'news_placeholder.jpg'),
+                },
+            ],
         }),
     ],
 };
