0698767ddf2a634749b6ddbe8330bb7ca215fc47
Alex <AlexiyKami@github.com>
2022-07-04 15:00:09 +0300

    refactor: change fetch URL

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '91be3949de5c458aa54231f1a8abf5e4', // получите свой ключ https://newsapi.org/
         });
     }
