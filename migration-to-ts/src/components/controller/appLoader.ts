c393e7aca8576b0d2075d874e4653523e51ee4b9
Alex <AlexiyKami@github.com>
2022-07-15 12:54:50 +0300

    refactor: declare constants for URL and APIkey

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -1,9 +1,12 @@
 import Loader from './loader';
 
+const urlLink = 'https://nodenews.herokuapp.com/';
+const apiKey = '91be3949de5c458aa54231f1a8abf5e4';
+
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
-            apiKey: '91be3949de5c458aa54231f1a8abf5e4', // получите свой ключ https://newsapi.org/
+        super(urlLink, {
+            apiKey: apiKey, // получите свой ключ https://newsapi.org/
         });
     }
 }
