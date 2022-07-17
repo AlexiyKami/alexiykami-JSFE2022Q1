30b024765de27f326ec2acb86802e68cf58a39a1
Alex <AlexiyKami@github.com>
2022-07-18 01:12:22 +0300

    feat: add objects in state.ts

---
+++ online-store/src/types/types.ts
@@ -0,0 +1,18 @@
+export interface IProduct {
+  id: number,
+  brand: string;
+  type: string;
+  name: string;
+  color: string;
+  year: number;
+  quantity: number;
+  price: number;
+  imgSrc: string;
+  isPopular: boolean,
+}
+
+export interface ISmartphone extends IProduct {
+  numberOfCameras: number;
+  RAM: string;
+  memory: string;
+}
