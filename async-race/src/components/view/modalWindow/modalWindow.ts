95899fd47376fe0ad7f85041ac524ea2a3ff2fb7
Alex <AlexiyKami@github.com>
2022-08-08 23:25:45 +0300

    feat: add modal window

---
+++ async-race/src/components/view/modalWindow/modalWindow.ts
@@ -0,0 +1,25 @@
+import AppController from '../../controller/controller';
+import './modalWindow.css';
+
+class ModalWindow {
+  controller: AppController;
+
+  constructor(controller: AppController) {
+    this.controller = controller;
+    this.controller.onModalOpen = this.draw.bind(this);
+    this.controller.onModalClose = this.clear.bind(this);
+  }
+
+  draw(message:string): void {
+    const wrapper = `<div class='modal'>
+      <div class='window'>${message}</div>
+    </div>`;
+    document.body.insertAdjacentHTML('beforeend', wrapper);
+  }
+
+  clear() {
+    document.querySelector('.modal')?.remove();
+  }
+}
+
+export default ModalWindow;
